623e5e482587d9a64cf7e4b46a8bbb37e71dab73
Eric Emerson <EricEmerson@users.noreply.github.com>
2020-07-05 16:42:19 -0400

    Added link button to info

--- inc/scr/info.php
+++ inc/scr/info.php
@@ -3,7 +3,7 @@
 <?php
 
     //Queries the database from projects.
-    $sql_command = "SELECT project_title, project_image1, project_image2, project_image3, project_tinyinfo, project_longinfo, project_language, project_team, project_complete, project_date FROM projects WHERE project_id = ?";
+    $sql_command = "SELECT project_title, project_image1, project_image2, project_image3, project_tinyinfo, project_longinfo, project_language, project_team, project_complete, project_date, project_button, project_link FROM projects WHERE project_id = ?";
     $sql_statement = mysqli_stmt_init($database_connection);
 
     //Checks if the statement can be prepared.
@@ -43,6 +43,9 @@
                 </div>
                 <div class=\"info_divider\">
                     <p class=\"info_longinfo\">".$sql_row["project_longinfo"]."</p>
+                </div>
+                <div class=\"info_divider\">
+                    <input class=\"info_button\" type=\"button\" onclick=\"location.href='".$sql_row["project_link"]."';\" value=\"".$sql_row["project_button"]."\" />
                 </div>
                 <div class=\"info_divider\">";
                 include("tags.php");
